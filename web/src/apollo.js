7a0e2402ae2bf2e34e16ff43fb98b023e41df4da
aadeel <aadeel@uoguelph.ca>
2020-08-24 00:37:55 -0400

    Updated for heroku deploy

--- web/src/apollo.js
+++ web/src/apollo.js
@@ -1,8 +1,5 @@
 import ApolloClient from 'apollo-boost';
 
-
-// "http://localhost:5000/graphql" use this as uri if changed api locally
-
 export default new ApolloClient({
-  uri: "https://blooming-inlet-37777.herokuapp.com/graphql",
+  uri: "http://localhost:5000/graphql",
 });
